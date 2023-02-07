6b2bde50871ead2fb73e3923e2e44febbbbc6a8d
Sofya Vibe <sofyavibe@gmail.com>
2023-02-07 21:27:42 +0700

    gallery component donnneeee

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CardA from './components/CardA/CardA';
 import CardB from './components/CardB/CardB';
 import Scroll from './components/Scroll/Scroll';
 import Footer from './components/Footer/Footer';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 function App() {
@@ -12,16 +13,19 @@ function App() {
 
 
   return (
-    <div>
-      <Header />
-      <main className='main'>
+    <div className='app_container'>
+      <ParallaxProvider>
+        <Header />
+      
         <CardA id={1}/>
         <CardB id={1}/>
         <Scroll />
         <CardA id={2}/>
         <CardB id={2}/>
-      </main>
-      <Footer />
+   
+        <Footer />
+      </ParallaxProvider>
+      
     </div>
   );
 }
--- src/components/Scroll/Scroll.jsx
+++ src/components/Scroll/Scroll.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import './scroll.css';
 import Card from "./Card";
 import Data from "./Data";
 
+
 const Scroll = () => {
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScroll(window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+    }, false);
+  }, []);
+  
 
   return (
     
     <div className="scroll">
       <h1 className="gallery__h1">LOREM IPSUM DOLOR SIT AMET</h1>
-      <div className="gallery">
-      
-      <div className="scroll__container">
+      <div className="gallery" >      
+        <div className="scroll__container" id="container" style={{ animationDelay: `calc(${scroll} * -1s)` }}>
         {Data.map((item) => {
           return (
             item ?
-            <Card item={item} key={item.id} />
+            <Card item={item} key={item.id}/>
             : <div className=""></div>
           )
         })}
       </div>
+    
     </div>
     </div>
   )
